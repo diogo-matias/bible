1582029b24f0271f884f36a5cac6ce09bb0bf17a
Diogo Matias <diogomaccedo200@gmail.com>
2023-06-13 23:24:52 -0300

    feat: remove logs

--- src/store/modules/bible/index.ts
+++ src/store/modules/bible/index.ts
@@ -147,19 +147,12 @@ export const nextOrPreviousChapter = createAsyncThunk(
                 (item: any) => item.id === selectedChapterInfo?.id
             ) ?? 0;
 
-        console.log("currentChapterIndex", currentChapterIndex);
-
         const currentBookIndex =
             books?.findIndex((item) => item.id === selectedBook?.id) ?? 0;
 
-        console.log("currentBookIndex", currentBookIndex);
-
         const newChapter = chapters[currentChapterIndex + sum];
 
-        console.log("newChapter", newChapter);
-
         if (!newChapter) {
-            console.log("entrou no if");
             const selectedBook = books[currentBookIndex + sum];
             dispatch(setSelectedBookThunk(selectedBook));
 
